b336e6dcda913d534d0678564313b53842d44e93
jooita <jiwoodidy@nate.com>
2018-04-19 09:41:16 +0900

    Update altibase.sh

--- test/altibase.sh
+++ test/altibase.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-go test -v -dsn TestOdbc -user keti -passwd $passwd -table odbctest
+go test -v -dsn TestOdbc -user $user -passwd $passwd -table odbctest
 
 cd altibase
 go test -v -dsn TestOdbc -table odbctest
